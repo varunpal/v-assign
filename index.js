1fb5d1c525b092f35d2fd69d2013e64e56c4447d
Varun <varun.pal@myntra.com>
2016-05-12 21:51:47 +0530

    Code cleanup.

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ module.exports = function () {
     for (index = 1; index < arguments.length; index++) {
       if (arguments[index] && typeof arguments[index] === 'object') {
         keys = Object.keys(arguments[index]);
-        console.log("keys are: ", keys);
         for (keyIndex = 0; keyIndex < keys.length; keyIndex++) {
-          console.log("Adding key: ", keys[keyIndex]);
           source[keys[keyIndex]] = arguments[index][keys[keyIndex]];
         }
       }
